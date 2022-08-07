21a2b238f0b05579b0743757290cb67397a39f58
3abqreno <102177769+3abqreno@users.noreply.github.com>
2022-08-07 15:23:15 +0200

    Create A 77-Is it rated?.cpp

---
+++ A 77-Is it rated?.cpp
@@ -0,0 +1,120 @@
+
+//   _____       _                                
+//  |____ |     | |                               
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___  
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \ 
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/ 
+//                       | |                     
+//                       |_|                     
+
+#include <bits/stdc++.h>
+#include<cmath>
+#include<string>
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+int dx[8] = { 0,0,1,-1,1,-1,1,-1 };
+int dy[8] = { 1,-1,0,0,1,-1,-1,1 };
+int M[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int Primes[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229 };//50 primes
+bool isPrime(int n)
+{
+	if (n == 2) return true;
+	if (n < 2 || n % 2 == 0) return false;
+
+	for (int i = 3; i * i <= n; i += 2)
+	{
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+long long gcd(int a, int b)
+{
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+long long lcm(int a, int b)
+{
+	if (a == 1 && b == 1)
+	{
+		return 1;
+	}
+	return (a / gcd(a, b)) * b;
+}
+
+long long fast_power(long long base, long long power) {
+	long long result = 1;
+	while (power > 0) {
+
+		if (power % 2 == 1) { // Can also use (power & 1) to make code even faster
+			result = (result * base) % MOD;
+		}
+		base = (base * base) % MOD;
+		power = power / 2; // Can also use power >>= 1; to make code even faster
+	}
+	return result;
+}
+ll factorial(ll n) {
+	if (n == 0)
+		return 1;
+	ll factorial = 1;
+	for (int i = 2; i <= n; i++)
+		factorial = factorial * i;
+	return factorial;
+}
+float nCr(float n, float r)
+{
+	int i, res;
+	if (r > 0)
+		return (n / r) * nCr(n - 1, r - 1);
+	else
+		return 1;
+}
+int n, m;
+
+vector<vector<int>>studens;
+vector<int>vis(100);
+int dfs(int parent, int count=1) {
+	vis[parent] = 1;
+	for (auto& i: studens[parent]) {
+		if (!vis[i]&&i!=parent&&!studens[parent][i])
+			count += dfs(i);
+	}
+	return count;
+}
+void solve() {
+	
+	int n,x,y;
+	cin >> n;
+	bool lower=0;
+	vector<int>ratings;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x >> y;
+		if (x != y) {
+			cout << "rated";
+			return;
+		}
+		ratings.push_back(x);
+		if (ratings.size()>1 && ratings[i] > ratings[i - 1]) {
+			lower = 1;
+		}
+	}
+	if (lower)cout << "unrated";
+	else cout << "maybe";
+	
+}
+
+int main() {
+	int t = 1; 
+	//cin >> t;
+	while (t--) {
+		solve();
+	}
+	
+}
