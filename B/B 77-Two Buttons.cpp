51163ac8dc4cd0c3e7e5179f8972b8edbf768a9b
3abqreno <102177769+3abqreno@users.noreply.github.com>
2022-08-15 19:20:47 +0200

    Create B 77-Two Buttons.cpp

---
+++ B/B 77-Two Buttons.cpp
@@ -0,0 +1,172 @@
+
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include<cmath>
+#include<string>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int Primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199,
+                211, 223, 227, 229};//50 primes
+bool isPrime(int n) {
+    if (n == 2) return true;
+    if (n < 2 || n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+long long gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base) % MOD;
+        }
+        base = (base * base) % MOD;
+        power >>= 1;
+    }
+    return result;
+}
+
+
+double nCr(double n, double r) {
+    int i, res;
+    if (r > 0)
+        return (n / r) * nCr(n - 1, r - 1);
+    else
+        return 1;
+}
+
+ll factorial(ll n) {
+    return (n == 1 || n == 0) ? 1 : n * factorial(n - 1);
+}
+
+ll fastpow(ll b, ll e) {
+    if (!e) {
+        return 1;
+    }
+    if (e & 1) {
+        return b * fastpow(b, e - 1) % MOD;
+    }
+    ll x = fastpow(b, e / 2) % MOD;
+    return x * x;
+}
+
+int binarySearch(vector<int> &numberrs, int value) {
+    int start = 0, end = numberrs.size() - 1, mid, index = -1;
+    while (start < end) {
+        mid = start + (end - start) / 2;
+        if (numberrs[mid] >= value) {
+            end = mid;
+
+        } else {
+            start = mid + 1;
+            index = mid;
+        }
+    }
+    return index;
+
+}
+int countDivisors(int n)
+{
+    int cnt = 0;
+    for (int i = 1; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            // If divisors are equal,
+            // count only one
+            if (n / i == i)
+                cnt++;
+
+            else // Otherwise count both
+                cnt = cnt + 2;
+        }
+    }
+    return cnt;
+}
+void solve() {
+    map<int,bool>seen;
+int n,m;
+cin>>n>>m;
+if(n==m){
+    cout<<0;
+    return;
+}
+queue<int>q;
+int count=0,count_2;
+if(2*n<2e4+1);
+q.push(2*n);
+if(n-1>0)
+q.push(n-1);
+int temp,move=1;
+count=q.size();
+while(q.size()){
+    for (int i = 0; i < count; ++i) {
+        temp=q.front();
+        q.pop();
+        if(temp==m){
+            cout<<move;
+            return;
+        }
+        if(!seen[temp*2]&&(temp*2)<2e4+1){
+            q.push(temp*2);
+            seen[temp*2]=1;
+            count_2++;
+        }
+        if(!seen[temp-1]&&(temp-1)<2e4+1){
+            q.push(temp-1);
+            seen[temp-1]=1;
+            count_2++;
+        }
+    }
+    count=count_2;
+    count_2=0;
+    move++;
+}
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
