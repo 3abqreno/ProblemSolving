9b2a2ab27c15dfb6ac57ff972f40a2397a712cf5
3abqreno <102177769+3abqreno@users.noreply.github.com>
2022-08-30 04:22:33 +0200

    Create B 126-Little Girl and Game.cpp

---
+++ B 126-Little Girl and Game.cpp
@@ -0,0 +1,77 @@
+
+    //   _____       _
+    //  |____ |     | |
+    //     / / __ _| |__   __ _ _ __ ___ _ __   ___
+    //      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+    //  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+    //  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+    //                       | |
+    //                       |_|
+    #include<iostream>
+    #include <bits/stdc++.h>
+    #include<cmath>
+    #include <unordered_map>
+
+    #define all(v) v.begin(),v.end()
+    #define sortv(v) sort(all(v))
+    #define F first
+    #define S second
+    using namespace std;
+    typedef long long ll;
+    const double pi = 3.14159265358979323846;
+    #define MOD 1000000007
+    #define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+    int dx[8] = {0, 0, 1, -1};
+    int dy[8] = {1, -1, 0, 0};
+
+    long long gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    ll fastpow(ll b, ll e) {
+        if (!e) {
+            return 1;
+        }
+        if (e & 1) {
+            return b * fastpow(b, e - 1);
+        }
+        ll x = fastpow(b, e / 2);
+        return x * x;
+    }
+
+
+    void solve() {
+        string s;
+        cin>>s;
+        bool pal=1,odd=0;
+        map<char,int>mp;
+        for (int i = 0; i < s.size(); ++i) {
+            mp[s[i]]++;
+        }
+        if(mp.size()>1) {
+            for (auto i: mp) {
+            if(i.second%2){
+                if(odd){
+                    pal=0;
+                    break;
+                }
+                odd=1;
+            }
+            }
+        }
+        if(pal||s.size()%2)cout<<"First";
+        else cout<<"Second";
+    }
+        int main() {
+    //    freopen("input.txt", "r", stdin);
+    //    freopen("output.txt", "w", stdout);
+            allam
+            int t = 1;
+    //    cin >> t;
+            while (t--) {
+                solve();
+            }
+
+        }
