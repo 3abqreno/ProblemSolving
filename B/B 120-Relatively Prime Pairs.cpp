3c39a91046fa7e3f74cfea4ff211d4d4a06e1e00
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-01 03:23:34 +0200

    Create B 120-Relatively Prime Pairs.cpp

---
+++ B/B 120-Relatively Prime Pairs.cpp
@@ -0,0 +1,69 @@
+
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+    //  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include<cmath>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll fastpow(ll b, ll e) {
+    if (!e) {
+        return 1;
+    }
+    if (e & 1) {
+        return b * fastpow(b, e - 1) % MOD;
+    }
+    ll x = fastpow(b, e / 2) % MOD;
+    return x * x % MOD;
+}
+
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+
+void solve() {
+    ll l,r;
+    cin>>l>>r;
+    cout<<"YES"<<endl;
+    for (ll i = l; i <=r ; i+=2) {
+        cout<<i<<" "<<i+1<<'\n';
+    }
+
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
