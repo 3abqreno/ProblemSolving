93347296f529a46b125f5b302beb76a428cf0722
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-04 04:19:43 +0200

    Create B 133-IP-TV(no submit yet).cpp

---
+++ B/B 133-IP-TV(no submit yet).cpp
@@ -0,0 +1,157 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+    //  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include<cmath>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll fastpow(ll b, ll e) {
+    if (!e) {
+        return 1;
+    }
+    if (e & 1) {
+        return b * fastpow(b, e - 1) % MOD;
+    }
+    ll x = fastpow(b, e / 2) % MOD;
+    return x * x % MOD;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+class DSU {
+    int* parent;
+    int* rank;
+
+public:
+    DSU(int n)
+    {
+        parent = new int[n];
+        rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+    }
+
+    // Find function
+    int find(int i)
+    {
+        if (parent[i] == -1)
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    // Union function
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 != s2) {
+            if (rank[s1] < rank[s2]) {
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else {
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+};
+
+class Graph {
+    vector<vector<int>> edgelist;
+    int V;
+
+public:
+    Graph(int V) { this->V = V; }
+
+    void addEdge(int x, int y, int w)
+    {
+        edgelist.push_back({ w, x, y });
+    }
+
+    ll kruskals_mst()
+    {
+        // 1. Sort all edges
+        sort(edgelist.begin(), edgelist.end());
+
+        // Initialize the DSU
+        DSU s(V);
+        int ans = 0;
+        for (auto edge : edgelist) {
+            int w = edge[0];
+            int x = edge[1];
+            int y = edge[2];
+
+            // Take this edge in MST if it does
+              // not forms a cycle
+            if (s.find(x) != s.find(y)) {
+                s.unite(x, y);
+                ans += w;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+void solve() {
+    ll n,m,w;
+    cin>>n>>m;
+    string a,b;
+    map<string,int>mp;
+    int s=0;
+    Graph gr(n);
+    for (int i = 0; i < m; ++i) {
+        cin>>a>>b>>w;
+        if(mp[a]==0){
+            mp[a]=s;s++;
+        }
+        if(mp[b]==0){
+            mp[b]=s;s++;
+        }
+        gr.addEdge(mp[a],mp[b],w);
+    }
+    cout<<gr.kruskals_mst();
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+     cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
