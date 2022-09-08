05a09170a62909a70fd0f2d3a0eb842f58738425
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-08 19:36:16 +0200

    Create 23-The Child and Set(not my solution).cpp

---
+++ C/23-The Child and Set(not my solution).cpp
@@ -0,0 +1,76 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include<cmath>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll fastpow(ll b, ll e) {
+    if (!e) {
+        return 1;
+    }
+    if (e & 1) {
+        return b * fastpow(b, e - 1) % MOD;
+    }
+    ll x = fastpow(b, e / 2) % MOD;
+    return x * x % MOD;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+
+void solve() {
+    ll s,l;
+    cin>>s>>l;
+    vector<ll>v;
+    for (int i = l; i >=1 ; i--) {
+        ll r=(i & -i);
+        if(s-i>=0){
+            s-=r;
+            v.push_back(i);
+        }
+    }
+    if(s)cout<<-1;
+    else{
+        cout<<v.size()<<endl;
+        for (int i = 0; i < v.size(); ++i) {
+            cout<<v[i]<<" ";
+        }
+    }
+}
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", std   out);
+    allam
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
