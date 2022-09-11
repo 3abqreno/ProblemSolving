98ccf6b7ab7db4b53792db5af353b2101bafbd06
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-12 00:45:06 +0200

    Create 32-To Carry or not to Carry.cpp

---
+++ C/32-To Carry or not to Carry.cpp
@@ -0,0 +1,61 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+int f = 0;
+
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+   while(cin>>a>>b){
+       cout<<(a^b);
+   }
+}
+
+int main() {
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
