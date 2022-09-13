d821f05c2384732b3159b7e3329ea843ddd58813
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-13 08:18:26 +0200

    Create 41-Sherlock and Probability.cpp

---
+++ C/41-Sherlock and Probability.cpp
@@ -0,0 +1,95 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base);
+        }
+        base = (base * base);
+        power >>= 1;
+    }
+    return result;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+
+void solve() {
+    ll n,k;
+    cin>>n>>k;
+    vector<char>v(n+10);
+    vector<int>v2(n+10,0);
+    for (int i = 0; i < n; ++i) {
+        cin>>v[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if(v[i]=='1'){
+            v2[max(ll(0),i-k)]++;
+            v2[min(n,i+k+1)]--;
+        }
+    }
+    for (int i = 1; i < n; ++i) {
+        v2[i]+=v2[i-1];
+    }
+    ll sum=0,gread=n*n,g;
+    for (int i = 0; i < n; ++i) {
+        if(v[i]=='1')sum+=v2[i];
+    }
+    g=gcd(gread,sum);
+    sum/=g;
+    gread/=g;
+    cout<<sum<<"/"<<gread<<'\n';
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
