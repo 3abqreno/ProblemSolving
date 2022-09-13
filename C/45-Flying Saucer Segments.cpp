acf9ee7ab27648c6e44dbcec075449dc3df2eb53
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-14 01:07:35 +0200

    Create 45-Flying Saucer Segments.cpp

---
+++ C/45-Flying Saucer Segments.cpp
@@ -0,0 +1,78 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+int n, m;
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base)%m;
+        }
+        base = (base * base)%m;
+        power >>= 1;
+    }
+    return result%m;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+
+void solve() {
+
+    ll sum = 2, p = 2;
+    cin >> n >> m;
+
+    cout<<((fast_power(3,n)-1)+m)%m;
+
+
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    //allam
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
