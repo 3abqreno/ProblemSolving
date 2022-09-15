b0ad3f9a3ab802fc0d134e90f155dbb07503b9ad
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-15 12:47:49 +0200

    Create 65-Jzzhu and Sequences.cpp

---
+++ C/65-Jzzhu and Sequences.cpp
@@ -0,0 +1,87 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base);
+        }
+        base = (base * base);
+        power >>= 1;
+    }
+    return result;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+
+void solve() {
+    int a, b, t, n,m;
+    cin >> a >> b >> n;
+    n %= 6;
+    if(!n)n=6;
+    if (a < 0)a += MOD;
+    if (b < 0)b += MOD;
+    for (int i = 0; i < n - 2; ++i) {
+        t = a;
+        a = b;
+        b = (a - t);
+        if (b > MOD)b -= MOD;
+        if (b < 0)b += MOD;
+    }
+    if (n == 1)cout << a;
+    else
+        cout << b;
+
+}
+
+
+int main() {
+    //freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
