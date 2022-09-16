6c2f1feb7a388c898d87166990ef5a45d0f80d66
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-16 20:56:11 +0200

    Create 82-Print Check.cpp

---
+++ C/82-Print Check.cpp
@@ -0,0 +1,93 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base);
+        }
+        base = (base * base);
+        power >>= 1;
+    }
+    return result;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    int n,m,k,x,y,z;
+    cin>>n>>m>>k;
+    vector<pair<int,int>>r(5001,{0,-1});
+    vector<pair<int,int>>c(5001,{0,-1});
+    for (int i = 0; i < k; ++i) {
+        cin>>x>>y>>z;
+        if(x==1){
+            r[--y]={z,i};
+        }
+        else{
+            c[--y]={z,i};
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if(r[i].second>c[j].second){
+                cout<<r[i].first;
+            }
+            else{
+                cout<<c[j].first;
+            }
+            cout<<" ";
+        }
+        cout<<'\n';
+    }
+}
+
+int main() {
+    //freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
