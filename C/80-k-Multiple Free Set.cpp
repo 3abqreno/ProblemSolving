0cfb0810b2be0dcab16fe88aeceae957913ae7ad
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-09-16 07:02:12 +0200

    Create 80-k-Multiple Free Set.cpp

---
+++ C/80-k-Multiple Free Set.cpp
@@ -0,0 +1,86 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+#include <unordered_map>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+#define F first
+#define S second
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+
+long long fast_power(long long base, long long power) {
+    long long result = 1;
+    while (power > 0) {
+
+        if (power % 2 == 1) {
+            result = (result * base);
+        }
+        base = (base * base);
+        power >>= 1;
+    }
+    return result;
+}
+
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    int n, k, remove = 0;
+    int x;
+    cin >> n >> k;
+    map<ll, bool> mp;
+    for (int i = 0; i < n; ++i) {
+        cin >> x;
+        mp[x] = 1;
+    }
+    if (k != 1) {
+        for (auto i: mp) {
+            if (i.second == 1 && mp.find(i.first * k) != mp.end()) {
+                mp[i.first * k] = 0;
+                remove++;
+            }
+        }
+    }
+    cout << n - remove;
+
+}
+
+int main() {
+    //freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    allam
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
