57fa4e70e8ab2307b48dc9d1cb6ceae2695f4183
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-10 00:11:42 +0200

    Create 83-Playing Cubes.cpp

---
+++ C/83-Playing Cubes.cpp
@@ -0,0 +1,61 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_set_ll tree<ll, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_multiset tree<ll , null_type ,  less_equal<ll> , rb_tree_tag , tree_order_statistics_node_update>
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 0};
+int dy[8] = {1, -1, 0, 0, 0};
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+
+void solve() {
+    int n, m, temp;
+    cin >> n >> m;
+    temp = min(n, m);
+    n=max(n,m);
+    m=temp;
+    cout<<n-1<<" "<<m;
+}
+
+int main() {
+    int t = 1;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
