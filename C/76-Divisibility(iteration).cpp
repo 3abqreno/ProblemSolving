20a746b80fc0d21837465b3f51f0aacb8ee05da0
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-09 22:40:11 +0200

    Create 76-Divisibility(iteration).cpp

---
+++ C/76-Divisibility(iteration).cpp
@@ -0,0 +1,75 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_set_ll tree<ll, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define ordered_multiset tree<ll , null_type ,  less_equal<ll> , rb_tree_tag , tree_order_statistics_node_update>
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+const double pi = 3.14159265358979323846;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 0};
+int dy[8] = {1, -1, 0, 0, 0};
+
+
+long long gcd(ll a, ll b) {
+    if (b == 0)
+        return a;
+    if (a == 0)
+        return b;
+    return gcd(b, a % b) % MOD;
+}
+
+long long lcm(int a, int b) {
+    if (a == 1 && b == 1) {
+        return 1;
+    }
+    return (a / gcd(a, b)) * b;
+}
+
+int MAX = 100 + 10;
+bool dp[10005][100+10];
+int arr[10005];
+
+void solve() {
+    memset(dp, 0, sizeof dp);
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    dp[1][(arr[0]%k+k) % k] = 1;
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            if (dp[i][j]) {
+                dp[i + 1][((j + arr[i]) % k + k) % k] = 1;
+                dp[i + 1][((j - arr[i]) % k + k) % k] = 1;
+            }
+        }
+    }
+    if (dp[n][0])cout << "Divisible\n";
+    else cout << "Not divisible\n";
+}
+
+int main() {
+    int t = 1;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
