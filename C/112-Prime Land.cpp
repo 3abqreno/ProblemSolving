401e464120cf8ca28f4bbcc5d6912dbfc77c86c3
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-17 06:34:59 +0200

    Update 112-Prime Land.cpp

--- C/112-Prime Land.cpp
+++ C/112-Prime Land.cpp
@@ -19,32 +19,33 @@ int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
 
 
 void solve() {
- string s;
- while(true){
-     getline(cin,s);
-     if(s=="0")return;
-     stringstream os;
-     os<<s;
-     int p,e,num=1;
-     while(os>>p>>e){
-         for (int i = 0; i < e; ++i) {
-             num*=p;
-         }
-     }
-     num--;
-     map<int,int>mp;
-     for (int i = 2; i*i<= num; ++i) {
-         while(num%i==0){
-             mp[i]++;
-             num/=i;
-         }
-     }
-     if(num>1)mp[num]++;
-     for(auto it=mp.rbegin();it!=mp.rend();it++){
-         cout<<it->first<<" "<<it->second<<" ";
-     }
-     cout<<'\n';
- }
+    string s;
+    while(true){
+        getline(cin,s);
+        if(s=="0")return;
+        stringstream os;
+        os<<s;
+        int p,e,num=1;
+        while(os>>p>>e){
+            for (int i = 0; i < e; ++i) {
+                num*=p;
+            }
+        }
+        num--;
+        map<int,int>mp;
+        for (int i = 2; i*i<= num; ++i) {
+            while(num%i==0){
+                mp[i]++;
+                num/=i;
+            }
+        }
+        if(num>1)mp[num]++;
+        for(auto it=mp.rbegin();it!=mp.rend();it++){
+            cout<<it->first<<" "<<it->second;
+            if(it!=(--mp.rend()))cout<<" ";
+        }
+        cout<<'\n';
+    }
 }
 
 int main() {
