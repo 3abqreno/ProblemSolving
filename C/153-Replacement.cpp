99956f7884a4b00552b8b8aec17b4c5ae4ca8b4a
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-19 03:39:06 +0200

    Create 153-Replacement.cpp

---
+++ C/153-Replacement.cpp
@@ -0,0 +1,52 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    if(n==1){
+        cout<<(v[0]>1?1:2);
+        return;
+    }
+    sortv(v);
+    cout<<1<<" ";
+    for (int i = 1; i <n-1; ++i) {
+        cout<<v[i-1]<<" ";
+    }
+    if(n>1)
+    {if(v[n-1]==1)cout<<2;
+    else cout<<v[n-2];}
+}
+
+int main() {
+    int t = 1;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
