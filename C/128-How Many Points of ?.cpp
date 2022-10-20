ceb4eefadcf6266f649f3a03f54d8a037f86efa1
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-20 23:57:51 +0200

    Create 128-How Many Points of ?.cpp

---
+++ C/128-How Many Points of ?.cpp
@@ -0,0 +1,40 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    ll a, b;
+    int c=1;
+    while (cin >> a >> b) {
+        if (!a)return;
+        cout <<"Case "<<c<<": " <<((a) * (a - 1) / 2) * (b * (b - 1) / 2) << '\n';
+        c++;
+    }
+}
+
+int main() {
+    int t = 1;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
