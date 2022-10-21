45523214df91b869a16876cc69c2b97c83a64fd6
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-10-21 15:39:36 +0200

    Create 29-Magical Array.cpp

---
+++ C2/29-Magical Array.cpp
@@ -0,0 +1,48 @@
+//   _____       _
+//  |____ |     | |
+//     / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include<iostream>
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+#define MOD 1000000007
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    int n;
+    cin >> n;
+    int x, prev, cnt = 1;
+    cin >> prev;
+    ll ans=1;
+    for (int i = 1; i < n; ++i) {
+        cin >> x;
+        if(x==prev)cnt++;
+        else{
+            prev=x;
+            cnt=1;
+        }
+        ans+=cnt;
+    }
+    cout<<ans;
+}
+
+int main() {
+    int t = 1;
+    //freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    allam
+//    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+}
