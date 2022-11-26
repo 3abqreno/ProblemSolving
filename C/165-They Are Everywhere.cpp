350ab52213a77e4259f25cd9f470ec14c6173a36
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2022-11-26 22:58:48 +0200

    Create 165-They Are Everywhere.cpp

---
+++ C/165-They Are Everywhere.cpp
@@ -0,0 +1,53 @@
+//   _____       _
+//  |____ |     | |
+//      / / __ _| |__   __ _ _ __ ___ _ __   ___
+//      \ \/ _` | '_ \ / _` | '__/ _ \ '_ \ / _ \
+//  .___/ / (_| | |_) | (_| | | |  __/ | | | (_) |
+//  \____/ \__,_|_.__/ \__, |_|  \___|_| |_|\___/
+//                       | |
+//                       |_|
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define all(v) v.begin(),v.end()
+#define sortv(v) sort(all(v))
+using namespace std;
+typedef long long ll;
+#define allam  ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
+int dy[8] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    map<char, int> mp;
+    set<char> st;
+    for (auto i: s)st.insert(i);
+    deque<char> q;
+    int ans = 1e9;
+    for (int i = 0,j=0; i < s.size(); ++i) {
+       while(j<n&&mp.size()<st.size())mp[s[j++]]++;
+       if(mp.size()==st.size())ans=min(j-i,ans);
+       if(!--mp[s[i]])mp.erase(s[i]);
+    }
+    cout << ans;
+
+}
+
+
+int main() {
+    int t = 1;
+    //   freopen("input.txt", "r", stdin);//freopen("output.txt", "w", stdout);
+    allam
+//    cin >> t;
+    while (t--) {
+        solve();
+        cout << '\n';
+    }
+
+}
