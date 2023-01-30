7c449feb3103122006c2cd35eb1d6c6ff4ed5c24
Abdelrahman Abdelfatah <102177769+3abqreno@users.noreply.github.com>
2023-01-30 12:05:11 +0200

    Update 53-Intersecting Line Segments.cpp

--- C2/53-Intersecting Line Segments.cpp
+++ C2/53-Intersecting Line Segments.cpp
@@ -25,45 +25,45 @@ pair<double, double> equ[10000];
 //f => ax
 //s => b
 bool  intersect(int i,int j){
-auto [x,y]=equ[i];
-auto [x2,y2]=equ[j];
-if(x==-1e9&&x2==-1e9)return 0;
-if(x==x2&&y==y2)return 0;
-if(x==-1e9){
-double yy=x2*y+y2;
-return (yy>=min(a[i][1],a[i][3])&&yy<=max(a[i][1],a[i][3]));
-}else if(y==-1e9){
-    double yy=x*y2+y;
-    return (yy>=min(a[j][1],a[j][3])&&yy<=max(a[j][1],a[j][3]));
-}
-double xx=((y2-y)/(x-x2));
-double yy=xx*x+y;
-if((yy>=min(a[i][1],a[i][3])&&yy<=max(a[i][1],a[i][3]))&&(yy>=min(a[j][1],a[j][3])&&yy<=max(a[j][1],a[j][3]))){
-    if((xx>=min(a[i][0],a[i][2])&&xx<=max(a[i][0],a[i][2]))&&(xx>=min(a[j][0],a[j][2])&&xx<=max(a[j][0],a[j][2]))){
-        return 1;
+    auto [x,y]=equ[i];
+    auto [x2,y2]=equ[j];
+    if(x==-1e9&&x2==-1e9)return 0;
+    if(x==x2&&y==y2)return 0;
+    if(x==-1e9){
+        double yy=x2*y+y2;
+        return (yy>=min(a[i][1],a[i][3])&&yy<=max(a[i][1],a[i][3])&&y>=min(a[j][0],a[j][2])&&y<=max(a[j][0],a[j][2]));
+    }else if(y==-1e9){
+        double yy=x*y2+y;
+        return (yy>=min(a[j][1],a[j][3])&&yy<=max(a[j][1],a[j][3])&&y2>=min(a[i][0],a[i][2])&&y2<=max(a[i][0],a[i][2]));
     }
-}
-return 0;
+    double xx=((y2-y)/(x-x2));
+    double yy=xx*x+y;
+    if((yy>=min(a[i][1],a[i][3])&&yy<=max(a[i][1],a[i][3]))&&(yy>=min(a[j][1],a[j][3])&&yy<=max(a[j][1],a[j][3]))){
+        if((xx>=min(a[i][0],a[i][2])&&xx<=max(a[i][0],a[i][2]))&&(xx>=min(a[j][0],a[j][2])&&xx<=max(a[j][0],a[j][2]))){
+            return 1;
+        }
+    }
+    return 0;
 
 }
 void solve() {
     int n;cin>>n;
-        for (int i = 0; i < n; ++i) {
-            cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3];
-            if (a[i][0] == a[i][2])equ[i] = {-1e9, a[i][0]};
-            else if (a[i][1] == a[i][3])equ[i] = {0, a[i][1]};
-            else {
-                equ[i].first=(a[i][1] - a[i][3]) / double(a[i][0] - a[i][2]);
-                equ[i].second=a[i][1]-equ[i].first*a[i][0];
-            }
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i][0] >> a[i][1] >> a[i][2] >> a[i][3];
+        if (a[i][0] == a[i][2])equ[i] = {-1e9, a[i][0]};
+        else if (a[i][1] == a[i][3])equ[i] = {0, a[i][1]};
+        else {
+            equ[i].first=(a[i][1] - a[i][3]) / double(a[i][0] - a[i][2]);
+            equ[i].second=a[i][1]-equ[i].first*a[i][0];
         }
-        int ans=n;
-        for (int i = 0; i < n; ++i) {
-            for (int j = i+1; j < n; ++j) {
-                ans+=2*intersect(i,j);
-            }
+    }
+    int ans=n;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            ans+=2*intersect(i,j);
         }
-        cout<<ans<<'\n';
+    }
+    cout<<ans<<'\n';
 }
 
 int main() {
